7914720452b23fb73db26ab788de4502616a3c6d
Daniel Leong <me@dhleong.net>
2019-08-09 10:50:58 -0400

    Improve error-boundary with auto-reloading in dev mode

--- src/leiningen/new/cljs_archetype/src/main/views/error_boundary.cljs
+++ src/leiningen/new/cljs_archetype/src/main/views/error_boundary.cljs
@@ -1,20 +1,43 @@
 (ns {{ns-name}}.views.error-boundary
   (:require [reagent.core :as r]))
 
+(when goog.DEBUG
+  ; in debug builds, we can auto-retry rendering error'd components
+  ; every time a load occurs
+  (def ^:private active-err-atoms (atom #{}))
+
+  (defn- ^:dev/after-load clear-errors []
+    (swap! active-err-atoms (fn [atoms]
+                              (doseq [a atoms]
+                                (reset! a nil))
+
+                              ; clear
+                              #{}))))
+
 (defn error-boundary [& _]
   (r/with-let [err (r/atom nil)]
     (r/create-class
-     {:display-name "Error Boundary"
+      {:display-name "Error Boundary"
 
-      :component-did-catch (fn [_this error info]
-                             (js/console.warn error info)
-                             (reset! err error))
+       :component-did-catch (fn [_this error info]
+                              (js/console.warn error info)
+                              (reset! err error))
 
-      :reagent-render (fn [& children]
-                        (if-let [err @err]
-                          [:div.error
-                           [:h1 "Oops! Something went wrong"]
-                           [:div (str err)]]
+       :statics
+       #js {:getDerivedStateFromError (fn [error]
+                                        (when goog.DEBUG
+                                          ; enqueue the atom for auto-clearing
+                                          (swap! active-err-atoms conj err))
 
-                          (into [:<>] children)))})))
+                                        (reset! err error))}
+
+       :reagent-render (fn [& children]
+                         (if-let [err @err]
+                           [:div.error
+                            [:h1 "Oops! Something went wrong"]
+                            [:pre (if (ex-message e)
+                                    (.-stack e)
+                                    (str e))]]
+
+                           (into [:<>] children)))})))
 
